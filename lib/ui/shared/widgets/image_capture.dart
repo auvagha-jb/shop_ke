db765878d762e2b0af7dc679ed1b28b63e0064b7
auvagha_jb <jerry.auvagha@gmail.com>
2020-12-08 01:13:41 +0300

    Empty Image Capture widget dependencies, Camera dependencies

---
+++ lib/ui/shared/widgets/image_capture.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shop_ke/core/view_models/camera_view_model.dart';
+import 'package:shop_ke/ui/views/general/base_view.dart';
+
+class ImageCapture extends StatefulWidget {
+  @override
+  _ImageCaptureState createState() => _ImageCaptureState();
+}
+
+class _ImageCaptureState extends State<ImageCapture> {
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<CameraViewModel>(
+      builder: (context, model, child) => Container(),
+    );
+  }
+}
